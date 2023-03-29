5937886d4ed0375b3d7ff3bef10f7d07a989761f
manuelspinto <manuelseixaspinto@gmail.com>
2023-03-29 18:15:34 +0100

    Added "final" parameter to automatically check the word until the final word is found

---
+++ TermoSolver/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using TermoSolver.Services.Solver;
+
+namespace TermoSolver.Extensions
+{
+    public static class StringExtensions
+    {
+        public static WordState[]? ToInitialWordState(this string? str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return null;
+            }
+
+            WordState[]? wordState = new WordState[str.Length];
+
+            for (int i = 0; i < wordState.Length; i++)
+            {
+                wordState[i] = new WordState() { Character = str[i], State = CharacterState.WrongCharacter };
+            }
+
+            return wordState;
+        }
+    }
+}
--- TermoSolver/Services/Solver/Models.cs
+++ TermoSolver/Services/Solver/Models.cs
@@ -40,6 +40,7 @@
 
     public enum CharacterState
     {
+        Unknown,
         WrongCharacter,
         RightPosition,
         WrongPosition,
